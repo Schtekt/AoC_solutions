ee15cd12d7408c05280c86d7b9a498840cfbcfd5
Tim Johansson <totimom@live.se>
2024-12-01 11:59:46 +0100

    Update main.cpp to only contain outcommented code

--- C++/src/main.cpp
+++ C++/src/main.cpp
@@ -1,8 +1,11 @@
 // Example usage
-#include "2023/Day1/Day1.h"
+// #include "<year>/Day<Day#>/Day<Day#>.h"
+#include <crtdbg.h>
 int main()
 {
-    AoCSolution_2023_1 day1;
-    day1.Solve();
+    _CrtSetDbgFlag ( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+    // AoCUtilityTool tool("Resources/Conf.txt");
+    // AoCSolution_<year>_<Day#> day(tool);
+    // day.Solve();
     return 0;
 }
