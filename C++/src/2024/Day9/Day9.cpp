39c843d8d70153f04a221c38a56e507827bec84e
Tim Johansson <totimom@live.se>
2024-12-09 20:57:00 +0100

    Improved C++ solution for 2024, day 9

--- C++/src/2024/Day9/Day9.cpp
+++ C++/src/2024/Day9/Day9.cpp
@@ -12,7 +12,7 @@ AoCSolution_2024_9::AoCSolution_2024_9():
 AoCDaySolution({"2024", "9"})
 {}
 
-std::string AoCSolution_2024_9::PartOne()
+std::vector<std::pair<uint8_t, uint8_t>> createDiskMap(std::string input)
 {
     char fileSize = 0;
     char freeSize = 0;
@@ -20,7 +20,7 @@ std::string AoCSolution_2024_9::PartOne()
     std::vector<std::pair<uint8_t, uint8_t>> diskMap;
 
     // Assumed the input is only one string
-    std::stringstream ss{m_input[0]};
+    std::stringstream ss{input};
 
     while(ss >> fileSize)
     {
@@ -32,6 +32,28 @@ std::string AoCSolution_2024_9::PartOne()
         diskMap.emplace_back(std::make_pair(fileSize - '0', freeSize - '0'));
     }
 
+    return diskMap;
+}
+
+size_t calculateChecksum(std::vector<std::pair<size_t, uint8_t>> fileSystem)
+{
+    size_t checksum = 0;
+    size_t multiplier = 0;
+    for(size_t i = 0; i < fileSystem.size(); i++)
+    {
+        for(size_t j = 0; j < fileSystem.at(i).second; j++)
+        {
+            checksum += multiplier * fileSystem.at(i).first;
+            multiplier++;
+        }
+    }
+    return checksum;
+}
+
+std::string AoCSolution_2024_9::PartOne()
+{
+    auto diskMap = createDiskMap(m_input.at(0));
+
     size_t backIndex = diskMap.size() - 1;
     uint8_t internalIndex = 0;
     std::vector<std::pair<size_t, uint8_t>> compressedFileSystem;
@@ -39,7 +61,7 @@ std::string AoCSolution_2024_9::PartOne()
     {
         if(backIndex == i)
         {
-            compressedFileSystem.emplace_back(std::make_pair(i, diskMap[i].first - internalIndex));
+            compressedFileSystem.emplace_back(std::make_pair(i, diskMap.at(i).first - internalIndex));
             break;
         }
         else if (backIndex < i)
@@ -47,17 +69,17 @@ std::string AoCSolution_2024_9::PartOne()
             break;
         }
 
-        compressedFileSystem.emplace_back(std::make_pair(i, diskMap[i].first));
+        compressedFileSystem.emplace_back(std::make_pair(i, diskMap.at(i).first));
 
         // Fill in from back of diskMap
         uint8_t filled = 0;
-        while(filled < diskMap[i].second)
+        while(filled < diskMap.at(i).second)
         {
-            uint8_t toFillFromCurrentBackIndex = std::min(diskMap[backIndex].first - internalIndex, diskMap[i].second - filled);
+            uint8_t toFillFromCurrentBackIndex = std::min(diskMap.at(backIndex).first - internalIndex, diskMap.at(i).second - filled);
             compressedFileSystem.emplace_back(std::make_pair(backIndex, toFillFromCurrentBackIndex));
             internalIndex += toFillFromCurrentBackIndex;
             filled += toFillFromCurrentBackIndex;
-            if(internalIndex == diskMap[backIndex].first)
+            if(internalIndex == diskMap.at(backIndex).first)
             {
                 internalIndex = 0;
                 backIndex--;
@@ -70,88 +92,69 @@ std::string AoCSolution_2024_9::PartOne()
         }
     }
 
-    // Calc checksum
-    size_t checksum = 0;
-    size_t multiplier = 0;
-    for(size_t i = 0; i < compressedFileSystem.size(); i++)
-    {
-        for(size_t j = 0; j < compressedFileSystem.at(i).second; j++)
-        {
-            size_t lastChecksum = checksum;
-            checksum += multiplier * compressedFileSystem.at(i).first;
-            multiplier++;
-        }
-    }
-    return std::to_string(checksum);
+    return std::to_string(calculateChecksum(compressedFileSystem));
 }
 
 std::string AoCSolution_2024_9::PartTwo()
 {
-    char fileSize = 0;
-    char freeSize = 0;
+    auto diskMap = createDiskMap(m_input.at(0));
 
-    std::vector<std::pair<uint8_t, uint8_t>> diskMap;
+    std::vector<size_t> placedIndices;
+    std::vector<size_t> freeIndices;
 
-    // Assumed the input is only one string
-    std::stringstream ss{m_input[0]};
-
-    while(ss >> fileSize)
+    for(size_t i = 0; i < diskMap.size(); i++)
     {
-        if(!(ss >> freeSize))
-        {
-            freeSize = '0';
-        }
-
-        diskMap.emplace_back(std::make_pair(fileSize - '0', freeSize - '0'));
+        freeIndices.emplace_back(i);
     }
 
     size_t frontIndex = 0;
-    std::vector<std::pair<size_t, uint8_t>> compressedFileSystem;
-    std::vector<size_t> indicesPlaced;
-    for(size_t frontIndex = 0; frontIndex < diskMap.size(); frontIndex++)
+    std::vector<std::pair<size_t, uint8_t>> compressedFileSystem1;
+    while(freeIndices.size() > 0)
     {
-        if(std::find(indicesPlaced.begin(), indicesPlaced.end(), frontIndex) == indicesPlaced.end())
+        if(std::find(placedIndices.begin(), placedIndices.end(), frontIndex) == placedIndices.end())
         {
-            compressedFileSystem.emplace_back(std::make_pair(frontIndex, diskMap[frontIndex].first));
-            indicesPlaced.emplace_back(frontIndex);
+            compressedFileSystem1.emplace_back(std::make_pair(frontIndex, diskMap.at(frontIndex).first));
+            placedIndices.emplace_back(frontIndex);
+            freeIndices.erase(std::find(freeIndices.begin(), freeIndices.end(), frontIndex));
         }
         else
         {
-            compressedFileSystem.emplace_back(std::make_pair(0, diskMap[frontIndex].first));
+            compressedFileSystem1.emplace_back(std::make_pair(0, diskMap.at(frontIndex).first));
         }
 
-        size_t backIndex = diskMap.size() - 1;
+        if(freeIndices.size() == 0)
+        {
+            break;
+        }
+
+        size_t freeIndicesIndex = freeIndices.size() - 1;
         uint8_t currentFilledSpace = 0;
-        // Find something that fits in this space
-        while(backIndex > frontIndex && currentFilledSpace < diskMap[frontIndex].second)
+        uint8_t spaceToFill = diskMap.at(frontIndex).second;
+
+        while(freeIndicesIndex >= 0 && currentFilledSpace < spaceToFill)
         {
-            if(diskMap[backIndex].first <= diskMap[frontIndex].second - currentFilledSpace
-            && std::find(indicesPlaced.begin(), indicesPlaced.end(), backIndex) == indicesPlaced.end())
+            size_t freeIndex = freeIndices.at(freeIndicesIndex);
+            if(diskMap.at(freeIndex).first <= spaceToFill - currentFilledSpace)
             {
-                compressedFileSystem.emplace_back(backIndex, diskMap[backIndex].first);
-                indicesPlaced.emplace_back(backIndex);
-                currentFilledSpace += diskMap[backIndex].first;
+                compressedFileSystem1.emplace_back(freeIndex, diskMap.at(freeIndex).first);
+                currentFilledSpace += diskMap.at(freeIndex).first;
+                placedIndices.emplace_back(freeIndex);
+                freeIndices.erase(freeIndices.begin() + freeIndicesIndex);
             }
-            backIndex--;
+            if(freeIndicesIndex == 0)
+            {
+                break;
+            }
+            freeIndicesIndex--;
         }
 
-        if(currentFilledSpace < diskMap[frontIndex].second)
+        if(currentFilledSpace < spaceToFill)
         {
-            compressedFileSystem.emplace_back(0, diskMap[frontIndex].second - currentFilledSpace);
+            compressedFileSystem1.emplace_back(0, spaceToFill - currentFilledSpace);
         }
+
+        frontIndex++;
     }
 
-    // Calc checksum
-    size_t checksum = 0;
-    size_t multiplier = 0;
-    for(size_t i = 0; i < compressedFileSystem.size(); i++)
-    {
-        for(size_t j = 0; j < compressedFileSystem.at(i).second; j++)
-        {
-            size_t lastChecksum = checksum;
-            checksum += multiplier * compressedFileSystem.at(i).first;
-            multiplier++;
-        }
-    }
-    return std::to_string(checksum);
+    return std::to_string(calculateChecksum(compressedFileSystem1));
 }
